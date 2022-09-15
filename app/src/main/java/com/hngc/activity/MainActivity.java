2c9501e339ba80d2f168513368cd0e6abea162ef
1902574396 <111944327+1902574396@users.noreply.github.com>
2022-09-16 01:37:24 +0800

    up

---
+++ app/src/main/java/com/hngc/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hngc.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hngc.health.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
